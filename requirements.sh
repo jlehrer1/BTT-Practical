038ef496bf50c699ebf9f94e82450872e0549ae3
Julian Lehrer <jmlehrer@ucsc.edu>
2020-07-01 11:29:56 -0700

    Updated requirements file, tested

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+colors="\033[0;32m"
 # Install the required packages for BTT-Practical
+if ! [ -x "$(command -v brew)" ]; then
+	echo -e "$colors Brew is not installed, installing Brew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+	echo -e "$colors Brew installed, continuing..."
+fi 
 
-echo "Requirements installed successfully ... reloading shell"
+echo -e "$colors Installing required brew packages..."
+brew install osx-cpu-temp
+brew install rbenv
+
+echo -e "$colors Setting up rbenv gem"
+rbenv install 2.7.1
+rbenv global 2.7.1
+
+echo -r "$colors Installing iStats"
+gem install iStats
+
+echo -r "$colors Requirements installed successfully ... reloading shell."
 exec $SHELL
